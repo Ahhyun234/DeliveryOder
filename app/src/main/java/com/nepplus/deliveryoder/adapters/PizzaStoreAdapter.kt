2561bb46b5b51d4501dda1f302c56d5f50cfb2cc
이아현 <suk03029@gamil.com>
2021-11-08 15:06:44 +0900

    리스트뷰가 뿌려진 데이터 소환

--- app/src/main/java/com/nepplus/deliveryoder/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/nepplus/deliveryoder/adapters/PizzaStoreAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.nepplus.deliveryoder.R
 import com.nepplus.deliveryoder.datas.StoreData
 import java.util.zip.Inflater
@@ -21,6 +23,13 @@ class PizzaStoreAdapter(val mContext = Context, val Resid:Int, val mList: ArrayL
             tempRow = mInflater.inflate(R.layout.store_list_item,null)
             }
         val row = tempRow!!
+
+
+        val data=mList[position]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val textStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+
         return row
 
     }
