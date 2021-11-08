62985324520657615fefbfe43de7cb09b460369b
이아현 <suk03029@gamil.com>
2021-11-08 16:55:35 +0900

    리절트를 가지고 복귀

--- app/src/main/java/com/nepplus/deliveryoder/EditNickNameActivity.kt
+++ app/src/main/java/com/nepplus/deliveryoder/EditNickNameActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.deliveryoder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,14 @@ class EditNickNameActivity : AppCompatActivity() {
     }
 
     fun setOnEvent(){
+
+        btnOK.setOnClickListener {
+            val inputNickname = edtNewNickName.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick",inputNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+        }
 
 
     }
