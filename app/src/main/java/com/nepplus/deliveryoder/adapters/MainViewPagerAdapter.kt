a4effd381c46017761a7c41201d66910b7260cec
이아현 <suk03029@gamil.com>
2021-11-08 12:18:55 +0900

    페이지 별 제목을 설정

--- app/src/main/java/com/nepplus/deliveryoder/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/deliveryoder/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,19 @@ import com.nepplus.deliveryoder.pragments.MyProfile
 import com.nepplus.deliveryoder.pragments.PizzaStoreList
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return when(position){
+
+            0-> "피자 주문"
+            else -> "내 정보 설정"
+        }
+
+
+    }
+
+
     override fun getCount(): Int {
         return 2
     }
