d7a5fafc0e55426320f9b0bf0cf1b219cdb082c0
이아현 <suk03029@gamil.com>
2021-11-08 12:52:23 +0900

    어댑터를 화면의 뷰 페이저와 연결

--- app/src/main/java/com/nepplus/deliveryoder/MainActivity.kt
+++ app/src/main/java/com/nepplus/deliveryoder/MainActivity.kt
@@ -2,8 +2,13 @@ package com.nepplus.deliveryoder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.deliveryoder.Fragments.PizzaStoreListFragment
+import com.nepplus.deliveryoder.adapters.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : ViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setValues(){
+
+        mViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+
 
 
 
