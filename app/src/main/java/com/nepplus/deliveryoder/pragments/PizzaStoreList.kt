a50e7b224bddac7df598707cc694088ab52ec62b
이아현 <suk03029@gamil.com>
2021-11-08 12:03:47 +0900

    피자가게 목록 프레그먼트 kt 파일에 초안 작업이 끝

--- app/src/main/java/com/nepplus/deliveryoder/pragments/PizzaStoreList.kt
+++ app/src/main/java/com/nepplus/deliveryoder/pragments/PizzaStoreList.kt
@@ -16,5 +16,12 @@ class PizzaStoreList: Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_pizza_store_list,container, false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
     }
 }
